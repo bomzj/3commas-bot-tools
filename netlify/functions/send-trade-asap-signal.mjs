4f45efdf908976ff94b178e523f4c11f7c10a56e
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2022-01-06 23:05:17 +0100

    add debug info to netlify function

--- netlify/functions/send-trade-asap-signal.mjs
+++ netlify/functions/send-trade-asap-signal.mjs
@@ -3,8 +3,9 @@ import TradeAsapSignal from '../../src/trade-asap-signal.js'
 export async function handler(event, context) {
   let {apiKey, apiSecret, botId, isPaperTrading} = JSON.parse(event.body)
   let signal = new TradeAsapSignal(apiKey, apiSecret, isPaperTrading)
+  console.log(event.body)
   await signal.tradeAllPairs(botId)
-  
+  console.log('before return')
   return {
     statusCode: 200,
     body: JSON.stringify({message: `Sent trade all pairs ASAP signal to 3Commas bot (id:${botId})`})
