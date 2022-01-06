967098eb24692eba1f8bfc1d6fac3373fc0261c1
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2022-01-06 22:59:37 +0100

    try to fix exceptions of netlify function during run time

--- netlify/functions/send-trade-asap-signal.mjs
+++ netlify/functions/send-trade-asap-signal.mjs
@@ -3,7 +3,7 @@ import TradeAsapSignal from '../../src/trade-asap-signal.js'
 export async function handler(event, context) {
   let {apiKey, apiSecret, botId, isPaperTrading} = JSON.parse(event.body)
   let signal = new TradeAsapSignal(apiKey, apiSecret, isPaperTrading)
-  signal.tradeAllPairs(botId)
+  await signal.tradeAllPairs(botId)
   
   return {
     statusCode: 200,
